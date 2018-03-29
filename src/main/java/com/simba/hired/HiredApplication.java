2f330f797e2e2069e8be2b029e92917c5af4e40b
zenzilesihle <sihlezen10@gmail.com>
2018-03-29 04:34:48 +0200

    Structure

---
+++ src/main/java/com/simba/hired/HiredApplication.java
@@ -0,0 +1,12 @@
+package com.simba.hired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiredApplication.class, args);
+	}
+}
